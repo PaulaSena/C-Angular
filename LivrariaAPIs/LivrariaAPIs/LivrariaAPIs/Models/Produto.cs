f6f52bdc0d8ce8fefad2d59ec4f7e2fa9b200b39
Ana Paula Sena da Silva <45314696+PaulaSena@users.noreply.github.com>
2022-10-24 15:08:44 -0300

    Criação da Pasta Model e Classe Produto.cs

---
+++ LivrariaAPIs/LivrariaAPIs/LivrariaAPIs/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace LivrariaAPIs.Models
+{
+    public class Produto
+    {
+        public string ID {get;set;} 
+        public string Nome {get;set;}
+        public double Preco { get; set; }
+        public int Qnt { get; set; }
+        public string Categoria { get; set; }
+        public string Img { get; set; }
+
+
+
+
+    }
+}
