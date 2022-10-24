d14837bd48b74d78183dd3554f1203457961a8d1
PaulaSena <senasilvaanapaula@gmail.com>
2022-10-24 14:07:58 -0300

    Criando o Projeto e a Clase Produto

---
+++ LivrariaAPIs/LivrariaAPIs/Model/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace LivrariaAPIs.Model
+{
+    public class Produto
+    {
+        public string ID { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Qtd { get; set; }
+        public string Categoria { get; set; }
+        public string Img { get; set; }
+
+    }
+}
